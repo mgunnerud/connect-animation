311a6d5291ac1f2d2f103b6dd221387107324621
Martin Gunnerud <mgunnerud@gmail.com>
2025-09-24 22:15:03 +0200

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Animation } from "./animation";
 export const App = () => {
   return (
     <Animation
+      // supports 2-4 words
       words={["Bra", "folk", "smarte", "løsninger"]}
       duration={2.2}
       inactiveBoxColor="#fff"
